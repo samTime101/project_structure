ea7793aaecaf39df022f6cfa1fae86ccdcad9ced
samTime101 <srijanregmi44@gmail.com>
2025-08-21 15:36:31 +0545

    working react signin

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Signup from './components/signup/Signup';
+import SignIn from './components/signin/Signin';
 
 const App = () => {
   return (
     <div>
         <Signup />
+        <SignIn />
     </div>
   );
 };
--- react_frontend/src/components/signin/Signin.js
+++ react_frontend/src/components/signin/Signin.js
@@ -1,11 +1,55 @@
-// SAMIPREGMI
-import React, { Component } from 'react';
+// REFERENCED FROM https://medium.com/@mranwarali/how-to-create-a-signup-form-in-react-using-axios-to-send-user-data-to-a-custom-api-endpoint-e6240d04a3f9
+// SAME AS Signup.js
+// SAMIP REGMI
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Signin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+    const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    setSuccess('');
+
+    try {
+        const response = await axios.post('http://localhost:8000/api/signin/', {
+        email:email,
+        password:password,
+        });
+
+        setSuccess(response.data.message);
+    } catch (err) {
+        setError('An error occurred while trying to sign in. Please try again later.');
+        console.error(err);
+    }
+    };
+
+
   return (
     <div>
-      <h1>testing123</h1>
+      <h2>Signin</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+        <label>Email:
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+        </label>
+        </div>
+        <div>
+        <label>Password:
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+        </label>
+        </div>
+        <button type="submit">Signin</button>
+      </form>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {success && <p style={{ color: 'green' }}>{success}</p>}
     </div>
   );
-}
+};
+
 export default Signin;
